fafedc1fa7cf30142ee22e85802af53f00b16e24
Victor Cordeiro <victorhugocordeiro1997@gmail.com>
2019-01-31 20:31:44 -0200

    atualizando
    Mudando a checagem do URL E NAME
    Identando o codigo

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -13,6 +13,7 @@ $(function() {
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
+ 
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
@@ -21,32 +22,38 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
+
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
-
 
         /* Escrevendo um teste 
          * Criando um objeto e definindo um nome
          * verificando se está vazio 
          */
 
-        it('Definindo NAME e URL', function(){
-            var defined = true;
-              /*
-                * Fazendo a checagem da variavel para que ela esteja de acordo
-                * Vai percorrer tanto URL como NAME 
-               */
+        it('Definindo  URL', function(){
+            /*
+            * Fazendo a checagem da variavel para que ela esteja de acordo
+            * Vai percorrer tanto URL 
+            * */
             for(let feed of allFeeds){
-                if(expect(feed).not.toBe(null) || expect(feed).not.toBe('') || expect(feed).toBeDefined()){
-                defined = false;
-                }
-            }
-            expect(defined).toBe(true);
+                expect(feed.url).toBeDefined(); //Garantindo que o URL esteja definido
+                expect(feed.url.length).not.toBe(0); //Garantindo que o URL nao esteja vazio
+            };
+        });
+
+        it('Definindo NAME', function(){
+            for(let feed of allFeeds){
+                expect(feed.name).toBeDefined(); //Garantindo que NAME esteja definido
+                expect(feed.name.length).not.toBe(0); //Garantindo que NAME nao esteja vazio
+            };
         });
     });
 
+    
+    
 
     /* verificando o id body 
      * está verificando se tem outro body
@@ -55,10 +62,9 @@ $(function() {
         it('default', function(){
             let menu = document.querySelector('body').classList;
             expect(menu).toContain('menu-hidden');
-
         }); 
         /*Verificando se o abre e fecha o menu*/
-            it('interacao', function(){
+        it('interacao', function(){
             $('.menu-icon-link').click();
             //Verificando o 1  click
             expect($('body').hasClass('menu-hidden')).toBeFalsy();
@@ -69,16 +75,17 @@ $(function() {
         });
     });
 
-
     /* Escrevendo o menu de entrada */
     describe('entrada', function(){
         beforeEach(function(done){
             loadFeed(0,done);
-        }) ;
+        });
+        
         it('aqui deve carregar o feed',function(){
-        expect($('.feed .entry').length).not.toBe(0);
+            expect($('.feed .entry').length).not.toBe(0);
+        });
     });
-});
+    
 /* Escrevendo um suite com o feed sobre novas */
 describe('Novo feed de seleção', function(){
     var meuFeed1, meuFeed2;
@@ -89,16 +96,14 @@ describe('Novo feed de seleção', function(){
     beforeEach(function(done){
         loadFeed(0,function(){
             meuFeed1 = $('.feed .entry').html();
-        loadFeed(1,function(){
-            meuFeed2 = $('.feed .entry').html();
-            done();
-        });
+            loadFeed(1,function(){
+                meuFeed2 = $('.feed .entry').html();
+                done();
+            });
         });
     });
     it('Selecionando as noticias', function(){
         expect(meuFeed1).not.toBe(meuFeed2);
     });
 });
-
-
 }());
